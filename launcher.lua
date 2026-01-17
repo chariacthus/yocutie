8e860830fb61e31981d44fa5b60390edda05cab1
chariacthus <163328317+chariacthus@users.noreply.github.com>
2026-01-17 04:21:06 +0100

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -1,4 +1,4 @@
 -- son
 if game.PlaceId == 1768619736534 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/chariacthus/reborn/refs/heads/main/arsenal.luau"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/chariacthus/reborn/refs/heads/main/arsenal.lua"))()
 end
