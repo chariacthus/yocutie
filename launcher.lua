e04cf3e286ecb8777361bb2bf0fd77e3b54feeee
chariacthus <163328317+chariacthus@users.noreply.github.com>
2026-01-17 04:17:27 +0100

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -1,1 +1,3 @@
-if game 1768619736534 load = loadstringhttpgameget(
+if game.PlaceId == 1768619736534 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/chariacthus/reborn/refs/heads/main/arsenal.luau"))()
+end
